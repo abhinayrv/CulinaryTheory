b778f783a2d583b78a95e2ddee6dfce544ad6054
Abhinay <vongurabhinayreddy@gmail.com>
2022-11-04 17:24:26 -0400

    updated register and login

--- src/controller/auth.js
+++ src/controller/auth.js
@@ -5,33 +5,43 @@ const User = mongoose.model('User');
 
 exports.authenticate = function(req, res) {
     console.log(`Login request`);
-  User.findOne({ email: req.body.username })
-  .exec(function(err, user) {
-    if (err) {
-        console.log("Some error in user find");
-        throw err;
-    }
+  
+  if (!req.body.username || !req.body.password) {
 
-    if (!user) {
-        console.log("User not found");
-        response.sendUnauthorized(res, "Incorrect username or password")
-    } else if (user) {
-      user.verifyPassword(req.body.password, function(err, isMatch) {
-        if (err) { 
-            console.log("Some error in password compare");
-            throw err
-        }
-        if (isMatch) {
-            var session = req.session;
-            session.user = {username: user.email, role:user.role};
-            response.sendSuccess(res, "Logged in successfully!");
-        } else {
-            console.log("Password did not match");
-            response.sendUnauthorized(res, "Incorrect username or password")
-        }
-      });
-    }
-  });
+    response.sendBadRequest(res, "Please check the fields enetered");
+    
+  } else {
+
+    User.findOne({ email: req.body.username })
+    .exec(function(err, user) {
+      if (err) {
+          console.log("Some error in user find");
+          throw err;
+      }
+
+      if (!user) {
+          console.log("User not found");
+          response.sendUnauthorized(res, "Incorrect username or password")
+      } else if (user) {
+        user.verifyPassword(req.body.password, function(err, isMatch) {
+          if (err) { 
+              console.log("Some error in password compare");
+              throw err
+          }
+          if (isMatch) {
+              var session = req.session;
+              session.user = {username: user.email, role:user.role};
+              response.sendSuccess(res, "Logged in successfully!");
+          } else {
+              console.log("Password did not match");
+              response.sendUnauthorized(res, "Incorrect username or password")
+          }
+        });
+      }
+    });
+
+  }
+  
 }
 
 exports.ensureAuthenticated = function(req, res, next) {
